e4fb66653fb4b973ee908ddc00740f31e17692be
Jacob Mischka <jacob@mischka.me>
2021-07-05 17:42:21 -0500

    Add initial TypeScript type definitions
    Works toward #28

---
+++ src/types.d.ts
@@ -0,0 +1,29 @@
+import { SvelteComponentTyped } from 'svelte';
+import flatpickr from 'flatpickr';
+
+export type HookProps = [ Date[], string, flatpickr.Instance ];
+
+declare module 'svelte-flatpickr' {
+  interface SvelteFlatpickrProps {
+	  value?: Date | string;
+	  formattedValue?: string;
+	  element?: HTMLInputElement | string;
+	  dateFormat?: string;
+	  options?: flatpickr.Options.Options;
+	  input?: HTMLInputElement | string;
+	  fp?: flatpickr.Instance;
+  }
+
+  class Flatpickr extends SvelteComponentTyped<SvelteFlatpickrProps, {
+	  change: CustomEvent<HookProps>,
+	  open: CustomEvent<HookProps>,
+	  close: CustomEvent<HookProps>,
+	  monthChange: CustomEvent<HookProps>,
+	  yearChange: CustomEvent<HookProps>,
+	  ready: CustomEvent<HookProps>,
+	  valueUpdate: CustomEvent<HookProps>,
+	  dayCreate: CustomEvent<HookProps>,
+  }, {}> {}
+
+  export default Flatpickr;
+}
